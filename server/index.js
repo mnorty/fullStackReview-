e3db13a28a57ca5f488218d371d6959a53fb1dd8
mnorty <mnorty@gmail.com>
2019-05-30 14:09:36 -0600

    basic Router set up

--- server/index.js
+++ server/index.js
@@ -25,3 +25,5 @@ massive(CONNECTION_STRING).then((db) => {
 app.post('/auth/register',auth_ctrl.register)
 app.post('/auth/login',auth_ctrl.login)
 app.get('/auth/details', auth_ctrl.getDetails)
+app.get('/auth/user', auth_ctrl.getUser)
+app.get('/auth/logout', auth_ctrl.logout)
---
+++ src/Components/Details.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Details extends Component {
+  render(){
+    return <h1>Details</h1>
+  }
+}
+
+export default Details
