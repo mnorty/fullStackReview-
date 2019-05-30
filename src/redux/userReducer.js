98ba7ad3bbdc5c7e5399384fc27a71a3010bf9fa
mnorty <mnorty@gmail.com>
2019-05-30 14:41:46 -0600

    No errors I think

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -6,4 +6,30 @@ const initialState = {
   id: null
 }
 
-function reducer()
+const UPDATE_USER = 'UPDATE_USER'
+const CLEAR_USER = 'CLEAR_USER'
+
+export function updateUser(user){
+  return {
+    type: UPDATE_USER,
+    payload: user
+  }
+}
+
+export function claerUser(){
+  return{
+    type:CLEAR_USER
+  }
+}
+
+function reducer(state = initialState, action){
+  switch(action.type){
+    case UPDATE_USER:
+      console.log(action.payload)
+      return state
+    default:
+      return state
+  }
+}
+
+export default reducer
